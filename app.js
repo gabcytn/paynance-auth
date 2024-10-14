88d5e62937ba1049e9056ed0f8ae79e4da56fd7c
gabcytn <cayetanogabriel03@gmail.com>
2024-10-14 13:34:44 +0800

    Created individual routes for login and register

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send({
-    message: "Hello, world",
-    ok: true,
-  });
-});
+const loginRoute = require("./routes/login");
+const registerRoute = require("./routes/register");
+
+app.use("/login", loginRoute);
+app.use("/register", registerRoute);
 
 app.listen(port, () => {
   console.log(`Server listening at port ${port}`);
