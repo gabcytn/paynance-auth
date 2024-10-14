b1a963945d367b23a354cf9f40a86b0304409970
gabcytn <cayetanogabriel03@gmail.com>
2024-10-14 13:50:32 +0800

    Fixed routing errors

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const loginRoute = require("./routes/login.js");
-const registerRoute = require("./routes/register.js");
+const loginRoute = require("./routes/login");
+const registerRoute = require("./routes/register");
 
 app.use("/login", loginRoute);
 app.use("/register", registerRoute);
