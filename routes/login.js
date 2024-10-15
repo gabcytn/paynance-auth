ccada80c0479f3a5a7b64744bcad6677a0fbae05
gabcytn <cayetanogabriel03@gmail.com>
2024-10-15 15:30:28 +0800

    Fixed unhandled unfound email error

--- routes/login.js
+++ routes/login.js
@@ -5,19 +5,28 @@ module.exports = (connection, bcrypt) => {
   router.post("/", (req, res) => {
     const body = req.body;
 
-    connection.query(
-      "SELECT password FROM users WHERE email = ?",
-      [body.email],
-      (err, rows) => {
-        bcrypt.compare(body.password, rows[0].password, (err, result) => {
-          if (result === true) {
-            res.status(200).json({ message: "ok" });
+    try {
+      connection.query(
+        "SELECT password FROM users WHERE email = ?",
+        [body.email],
+        (err, rows) => {
+          if (rows.length !== 0) {
+            bcrypt.compare(body.password, rows[0].password, (err, result) => {
+              if (result === true) {
+                res.status(200).json({ message: "ok" });
+                return;
+              } else {
+                res.status(401).json({ message: "failed" });
+              }
+            });
           } else {
-            res.status(401).json({ message: "failed" });
+            res.status(404).json({ message: "failed" });
           }
-        });
-      }
-    );
+        }
+      );
+    } catch (e) {
+      console.error(e);
+    }
   });
 
   return router;
