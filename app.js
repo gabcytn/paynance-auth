4d5d79262247890f6b19aac03b98a2b5376f06b1
gabcytn <cayetanogabriel03@gmail.com>
2024-10-15 11:09:43 +0800

    Included body parser for request bodies

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
 const loginRoute = require("./routes/login");
 const registerRoute = require("./routes/register");
 
+app.use(bodyParser.json());
 app.use("/login", loginRoute);
 app.use("/register", registerRoute);
 
