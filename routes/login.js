494eefb02584f6c563d1fa993658e05f2854ebbc
gabcytn <cayetanogabriel03@gmail.com>
2024-11-30 00:14:22 +0800

    Refactor for better code readability

--- routes/login.js
+++ routes/login.js
@@ -9,20 +9,21 @@ module.exports = (connection, bcrypt) => {
       connection.query(
         "SELECT id, password FROM users WHERE email = ?",
         [body.email],
-        (err, rows) => {
-          if (rows.length !== 0) {
-            bcrypt.compare(body.password, rows[0].password, (err, result) => {
-              if (result === true) {
-                res.status(200).json({ id: rows[0].id });
-                return;
-              } else {
-                res.status(401).json({ message: "failed" });
-              }
-            });
-          } else {
+        (_, rows) => {
+          if (rows.length === 0) {
             res.status(404).json({ message: "failed" });
+            return;
           }
-        }
+
+          bcrypt.compare(body.password, rows[0].password, (_, result) => {
+            if (result) {
+              res.status(200).json({ id: rows[0].id });
+              return;
+            }
+          });
+
+          res.status(401).json({ message: "failed" });
+        },
       );
     } catch (e) {
       console.error(e);
